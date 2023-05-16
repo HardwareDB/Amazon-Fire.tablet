25623ecf76b875ce60e435811eae3d680597da04
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2023-05-16 01:57:21 -0500

    Update installation.sh

--- HD 8/8th Generation/os:/postmarketOS/installation.sh
+++ HD 8/8th Generation/os:/postmarketOS/installation.sh
@@ -4,3 +4,5 @@ pmbootstrap install --android-recovery-zip --recovery-install-partition=data
 
 # Make sure you're booted into TWRP at this point
 pmbootstrap flasher --method=adb sideload
+
+#from: https://wiki.postmarketos.org/wiki/Amazon_Fire_HD_8_2018_(amazon-karnak)
